391687c00d231f7d192af4f546c8f2fd5ff68429
dedecanton <andrecanton18@gmail.com>
2022-03-26 17:23:10 -0300

    nav component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
   return (
@@ -12,9 +13,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      {/* Nav */}
+      <Nav/>
       {/* Results */}
     </div>
   );
