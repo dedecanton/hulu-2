a81c65e4c8880e320b91964ef832b520c3ff8ed6
dedecanton <andrecanton18@gmail.com>
2022-03-26 20:31:03 -0300

    request utils added

--- utils/requests.ts
+++ utils/requests.ts
@@ -2,7 +2,11 @@
 
 const API_KEY = process.env.API_KEY;
 
-export default {
+type Results = {
+    [key:string]: any
+}
+
+const results:Results= {
     fetchTrending: {
         title: 'Trending',
         url: `/trending/all/week?api_key=${API_KEY}&language=en-US`
@@ -48,3 +52,5 @@ export default {
         url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
     },
 }
+
+export default results
