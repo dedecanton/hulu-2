a444beb098c296afcb3e9afda9d0f99988bfb476
dedecanton <andrecanton18@gmail.com>
2022-03-26 15:24:28 -0300

    responsive header completed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,35 @@
+import {
+  HomeIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
+
 import Image from "next/image";
 import React from "react";
+import HeaderItem from "./HeaderItem";
+
+const headerItems = [
+  {title: 'HOME', icon: HomeIcon},
+  {title: 'TRENDING', icon: LightningBoltIcon},
+  {title: 'VERIFIED', icon: BadgeCheckIcon},
+  {title: 'COLLECTIONS', icon: CollectionIcon},
+  {title: 'SEARCH', icon: SearchIcon},
+  {title: 'ACCOUNT', icon: UserIcon},
+]
 
 function Header() {
   return (
-    <header className="">
-      <h1>Header</h1>
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        {headerItems.map((item, index) => (
+          <HeaderItem key={index} title={item.title} Icon={item.icon}/>
+        ))}
+      </div>
       <Image
-        className='object-contain'
+        className="object-contain"
         src="https://links.papareact.com/ua6"
         width={200}
         height={100}
